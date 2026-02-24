e11b5c19684b33107cd3160d314da0ad2dc66230
Mostafa Hussien <mabozeed70@gmail.com>
2026-02-24 14:51:21 +0200

    updated task type to match new json server id generation

--- src/types/task.ts
+++ src/types/task.ts
@@ -2,7 +2,7 @@ export type TaskStatus = "backlog" | "in-progress" | "review" | "done";
 export type TaskPriority = "low" | "medium" | "high";
 
 export interface Task {
-  id: number;
+  id: string;
   title: string;
   description: string;
   status: TaskStatus;
